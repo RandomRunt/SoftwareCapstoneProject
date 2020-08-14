1b42d47bbe03072716af091c1aa2f263f9653caf
NathanRoland <54428606+NathanRoland@users.noreply.github.com>
2020-08-14 12:07:29 +1000

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/test')
+def test():
+    return render_template('another test.html')
+
+
 if __name__ == '__main__':
     app.run()
