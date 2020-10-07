666c25a9d058ad5db40481612860f31ff2637d4e
NathanRoland <54428606+NathanRoland@users.noreply.github.com>
2020-10-07 16:02:43 +1100

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -4,10 +4,10 @@ import sqlite3
 def createTable():
     conn = sqlite3.connect('properties.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE property_ids(property_id text, address text, price integer, coordinate text, 
-    domain_id text)''')
-    c.execute('''CREATE TABLE property_features(property_type text, bedrooms integer, bathrooms integer, car_spaces 
-    integer, )''')
+    # c.execute('''CREATE TABLE property_ids(property_id text, address text, price integer, coordinate text,
+    # domain_id text)''')
+    # c.execute('''CREATE TABLE property_features(property_type text, bedrooms integer, bathrooms integer, car_spaces
+    # integer, )''')
     c.execute('''CREATE TABLE suburbs(suburb_id text, suburb text, age_0_to_4 integer, age_5_to_19 integer, age_20_to_39 
     integer, age_40_to_59 integer, age_60_plus integer, postcode integer, state text, properties_sold integer, 
     clearance_rate integer, median_sale integer, total_sale integer)''')
