3d2586d3fa34a183bb4ea125af955582c4c595d2
NathanRoland <54428606+NathanRoland@users.noreply.github.com>
2020-10-08 15:29:27 +1100

    Update data_base.py

--- data_base.py
+++ data_base.py
@@ -4,24 +4,25 @@ import sqlite3
 def createTable():
     conn = sqlite3.connect('properties.db')
     c = conn.cursor()
-    # c.execute('''CREATE TABLE property_ids(property_id text, address text, price integer, coordinate text,
-    # domain_id text)''')
-    # c.execute('''CREATE TABLE property_features(property_type text, bedrooms integer, bathrooms integer, car_spaces
-    # integer, )''')
-    c.execute('''CREATE TABLE suburbs(suburb_id text, suburb text, age_0_to_4 integer, age_5_to_19 integer, age_20_to_39 
-    integer, age_40_to_59 integer, age_60_plus integer, postcode integer, state text, properties_sold integer, 
-    clearance_rate integer, median_sale integer, total_sale integer)''')
+    c.execute('''CREATE TABLE properties_data_base(property_id text, street_name text, street_num integer, suburb text, 
+    street_type text, state text, lower_price integer, upper_price integer, mid_price integer, image text, 
+    lat_coordinate text, long_coordinate text, property_type text, bedrooms integer, bathrooms integer, 
+    car_spaces integer  )''')
+
     conn.commit()
     conn.close()
 
 
-def addProperty(property_id, address, price, coordinate, domain_id, property_type, bedrooms, bathrooms, car_spaces):
+def addProperty(property_id, street_name, street_num, suburb, street_type, state, lower_price, upper_price, mid_price,
+                image, lat_coordinate, long_coordinate, property_type, bedrooms, bathrooms, car_spaces):
     conn = sqlite3.connect('properties.db')
     c = conn.cursor()
-    c.execute("INSERT INTO property_ids (property_id, address, price, coordinate, domain_id) VALUES (?, ?)",
-              (property_id, address, price, coordinate, domain_id))
-    c.execute("INSERT INTO property_features (property_type, bedrooms, bathrooms, car_spaces) VALUES (?, ?)",
-              (property_type, bedrooms, bathrooms, car_spaces))
+    c.execute("INSERT INTO properties_data_base (property_id, street_name, street_num, suburb, street_type, state, "
+              "lower_price, upper_price, mid_price, image, lat_coordinate, long_coordinate, property_type, bedrooms, "
+              "bathrooms, car_spaces) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+              (property_id, street_name, street_num, suburb, street_type, state, lower_price, upper_price, mid_price,
+               image, lat_coordinate, long_coordinate, property_type, bedrooms, bathrooms, car_spaces))
+
     conn.commit()
     conn.close()
 
@@ -47,4 +48,14 @@ def findSuburb(suburb):
     for row in c.execute('SELECT * FROM suburbs WHERE suburb=?', (suburb,)):
         houses_in_suburbs.append(row)
     return houses_in_suburbs
+    conn.close()
+
+
+def findProperty(property_id):
+    conn = sqlite3.connect('properties.db')
+    c = conn.cursor()
+    property = []
+    for row in c.execute('SELECT * FROM properties_data_base WHERE suburb=?', (property_id,)):
+        property.append(row)
+    return property
     conn.close()
